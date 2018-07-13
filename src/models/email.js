1111913cae909b47f22d8efc73090e424dba4128
Wahyu Budi Saputra <wahyubucil@gmail.com>
2018-07-13 14:19:16 +0800

    Create simple model

---
+++ src/models/email.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const emailSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        validate: (value) => {
+            return validator.isEmail(value);
+        }
+    }
+});
+
+module.exports = mongoose.model('Email', emailSchema);
